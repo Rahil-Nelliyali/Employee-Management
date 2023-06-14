3a19b3b77fb8e4b5150d5d03cdc6cc49fbf42ff4
Rahil-Nelliyali <rahilnelliyali0@gmail.com>
2023-06-14 16:50:27 +0530

    Employee delete functionality added

--- employee_management/emp_app/urls.py
+++ employee_management/emp_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('all', views.all, name='all'),
     path('add', views.add, name='add'),
     path('remove', views.remove, name='remove'),
+    path('remove/<int:emp_id>', views.remove, name='remove'),
     path('filter', views.filter, name='filter'),
 ]
--- employee_management/emp_app/views.py
+++ employee_management/emp_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+
+from datetime import datetime
+from django.shortcuts import render, HttpResponse
 from . models import Employee, Role, Department
 # Create your views here.
 
@@ -13,10 +15,38 @@ def all(request):
     return render(request, 'view.html', context)
 
 def add(request):
-    return render(request, 'add.html')
+    if request.method== "POST":
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        salary = int(request.POST['salary'])
+        bonus = int(request.POST['bonus'])
+        phone = int(request.POST['phone'])
+        dept = int(request.POST['dept'])
+        role = int(request.POST['role'])
+        new_emp = Employee(first_name=first_name,last_name=last_name,salary=salary,bonus=bonus,dept_id=dept,role_id=role,phone=phone,hire_date = datetime.now())
+        new_emp.save()
 
-def remove(request):
-    return render(request, 'remove.html')
+        return HttpResponse('Employee Added successfully')
+    elif request.method == "GET":
+        return render(request, "add.html")
+    else:
+        return HttpResponse("An exception occured! Employee has not been added!")
+
+    
+
+def remove(request, emp_id = 0):
+    if emp_id:
+        try:
+            emp_delete = Employee.objects.get(id = emp_id)
+            emp_delete.delete()
+            return HttpResponse("Delete successful!")
+        except:
+            return HttpResponse("Oops! Something gone wrong.")
+    emps = Employee.objects.all()
+    context = {
+        'emps' : emps
+    }
+    return render(request, 'remove.html', context)
 
 def filter(request):
     return render(request, 'filter.html')
